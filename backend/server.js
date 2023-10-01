d045471c736ac494d361bf44c830902f539d38a0
Avaneesh <rayavaneesh35305@gmail.com>
2023-10-01 19:27:22 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const app = require("./app");
+//const { connectDatabase } = require("./config/database");
+const cloudinary = require("cloudinary");
+const mongoose = require("mongoose");
+
+connectDatabase = async () => {
+  await mongoose
+    .connect(process.env.MONGO_URI)
+    .then((con) => console.log(`Database Connected: ${con.connection.host}`))
+    .catch((err) => console.log(err));
+};
+
+connectDatabase();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
